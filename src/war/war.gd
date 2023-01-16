6f76f384f7c98fead2d2e24d99f8f0cc5a1239be
NateWebber <nwebber@u.rochester.edu>
2023-01-16 13:47:55 -0500

    spawn deck on screen, modify ui

--- src/war/war.gd
+++ src/war/war.gd
@@ -7,15 +7,18 @@ enum Suit {CLUBS, DIAMONDS, HEARTS, SPADES}
 var current_deck
 var current_card
 
+const UICard = preload("res://src/cards-core/UICard.tscn")
+
+onready var DeckSpawnPos = $DeckSpawnPoint
+
 func _ready():
 	randomize()
 	current_deck = Deck.new()
 	current_card = current_deck.deal()
-	update_card_ui()
+	draw_ui_deck()
  
 func _on_DealButton_pressed():
 	current_card = current_deck.deal()
-	update_card_ui()
 
 func update_card_ui():
 	# card textures follow format of card[SUIT][RANK].png, where rank is either the number of initial of the face card's
@@ -36,5 +39,12 @@ func update_card_ui():
 	var texture_filename = "card%s%s.png" % [Suit.keys()[current_card.suit].capitalize(), initial]
 	current_card.print_name()
 	print("trying for filename: " + "res://sprites/cards/" + texture_filename)
-	$UI/CardImage.texture = load("res://sprites/cards/" + texture_filename) 
+	$UI/MasterContainer/GameContainer/CardImage.texture = load("res://sprites/cards/" + texture_filename) 
+	
+func draw_ui_deck():
+	for i in range (52):
+		var newUICard = UICard.instance()
+		newUICard.set_position(Vector2(DeckSpawnPos.position.x + (i * 0.5), DeckSpawnPos.position.y))
+		add_child(newUICard)
+	pass
 
