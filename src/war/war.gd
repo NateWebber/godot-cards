9f389e9fab631e53317d0ca5c7e5cb45335cf50b
NateWebber <nwebber@u.rochester.edu>
2023-01-22 20:44:09 -0500

    rudimentary dealing animation, looks a bit funky so may change later

--- src/war/war.gd
+++ src/war/war.gd
@@ -7,15 +7,29 @@ enum Suit {CLUBS, DIAMONDS, HEARTS, SPADES}
 var current_deck
 var current_card
 
+var player_one_ui_deck = []
+var player_two_ui_deck = []
+
 const UICard = preload("res://src/cards-core/UICard.tscn")
 
 onready var DeckSpawnPos = $DeckSpawnPoint
+onready var DeckDealAnchor1 = $DeckDealAnchor1
+onready var DeckDealAnchor2 = $DeckDealAnchor2
+onready var P1DeckAnchor = $P1DeckAnchor
+onready var P2DeckAnchor = $P2DeckAnchor
+
+export var CARD_ANIM_SPEED = 20
+
+signal deck_spawn_anim_complete
 
 func _ready():
 	randomize()
 	current_deck = Deck.new()
 	current_card = current_deck.deal()
-	draw_ui_deck()
+	do_start_animations()
+	
+func _wait(seconds):
+	print("waiting")
  
 func _on_DealButton_pressed():
 	current_card = current_deck.deal()
@@ -41,10 +55,53 @@ func update_card_ui():
 	print("trying for filename: " + "res://sprites/cards/" + texture_filename)
 	$UI/MasterContainer/GameContainer/CardImage.texture = load("res://sprites/cards/" + texture_filename) 
 	
+	
+func do_start_animations():
+	draw_ui_deck()
+	yield(self, "deck_spawn_anim_complete")
+	do_deck_split_anim()
+	
 func draw_ui_deck():
-	for i in range (52):
+	for i in range(52):
 		var newUICard = UICard.instance()
 		newUICard.set_position(Vector2(DeckSpawnPos.position.x + (i * 0.5), DeckSpawnPos.position.y))
 		add_child(newUICard)
+		if (i < 26):
+			player_one_ui_deck.append(newUICard)
+		else:
+			player_two_ui_deck.append(newUICard)
+	#first animation stage, fly up from bottom
+	while player_two_ui_deck[25].position.y > DeckDealAnchor1.position.y:
+		#move all cards before yield so that animation is simultaneous
+		for i in range(26):
+			var p1_card = player_one_ui_deck[i]
+			var p2_card = player_two_ui_deck[i]
+			p1_card.position.y -= CARD_ANIM_SPEED
+			p2_card.position.y -= CARD_ANIM_SPEED
+		yield(get_tree().create_timer(0.01), "timeout")
+	#second animation stage, fly over to left of screen
+	while player_two_ui_deck[25].position.x > DeckDealAnchor2.position.x:
+		#move all cards before yield so that animation is simultaneous
+		for i in range(26):
+			var p1_card = player_one_ui_deck[i]
+			var p2_card = player_two_ui_deck[i]
+			p1_card.position.x -= CARD_ANIM_SPEED
+			p2_card.position.x -= CARD_ANIM_SPEED
+		yield(get_tree().create_timer(0.01), "timeout")
+	#signal this animation is done
+	emit_signal("deck_spawn_anim_complete")
+	pass
+	
+func do_deck_split_anim():
+	for i in range(26):
+		var p1_card = player_one_ui_deck[i]
+		var p2_card = player_two_ui_deck[i]
+		#redo horizontal adjustment
+		p1_card.position.x = P1DeckAnchor.position.x + (0.5 * i)
+		p2_card.position.x = P2DeckAnchor.position.x + (0.5 * i)
+		while (p1_card.position.y < P1DeckAnchor.position.y):
+			p1_card.position.y += CARD_ANIM_SPEED
+			p2_card.position.y -= CARD_ANIM_SPEED
+			yield(get_tree().create_timer(0.01), "timeout")
 	pass
 
